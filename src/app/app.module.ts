c12c3f5c755b16a5d288c877ea05a3004da924ee
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-09 20:11:09 -0500

    feat(side-nav): added side navigation section and component source list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ArticleListComponent } from './news/components/article-list/article-list.component';
 import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
+import { SideNavigationBarComponent } from './public/components/side-navigation-bar/side-navigation-bar.component';
+import { SourceListItemComponent } from './news/components/source-list-item/source-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleListComponent,
-    FooterContentComponent
+    FooterContentComponent,
+    SideNavigationBarComponent,
+    SourceListItemComponent
   ],
   imports: [
     BrowserModule,
