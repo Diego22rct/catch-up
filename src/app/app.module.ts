2b01029a9cbc9fb660701e84b8d7018e05d6e0cb
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-09 20:21:22 -0500

    refactor: removed unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from "@angular/common/http";
 import { ArticleListComponent } from './news/components/article-list/article-list.component';
+import { LogoApiService } from "./shared/services/logo-api.service";
+import { NewsApiService } from "./news/services/news-api.service";
 import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
 import { SideNavigationBarComponent } from './public/components/side-navigation-bar/side-navigation-bar.component';
-import { SourceListItemComponent } from './news/components/source-list-item/source-list-item.component';
+import { NgOptimizedImage } from "@angular/common";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatMenuModule } from "@angular/material/menu";
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleListComponent,
     FooterContentComponent,
-    SideNavigationBarComponent,
-    SourceListItemComponent
+    SideNavigationBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    NgOptimizedImage
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    LogoApiService, NewsApiService
   ],
   bootstrap: [AppComponent]
 })
